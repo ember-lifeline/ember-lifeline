4b80ec4bff0aec23579e0e069bd5447ce6bf436d
Robert Jackson <rwjblue@twitch.tv>
2016-10-30 22:32:54 -0400

    Add eslint-plugin-ember-suave...

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,10 @@ module.exports = {
     ecmaVersion: 6,
     sourceType: 'module'
   },
-  extends: 'eslint:recommended',
+  extends: [
+    'eslint:recommended',
+    'plugin:ember-suave/recommended'
+  ],
   env: {
     'browser': true
   },
