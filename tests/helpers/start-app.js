d86347ed513aeb1eb3a042cfda6b6f5c988e7c3f
Robert Jackson <rwjblue@twitch.tv>
2016-10-30 22:43:33 -0400

    Fix failures identified by suave.

--- tests/helpers/start-app.js
+++ tests/helpers/start-app.js
@@ -2,13 +2,17 @@ import Ember from 'ember';
 import Application from '../../app';
 import config from '../../config/environment';
 
+const { merge, run } = Ember;
+
 export default function startApp(attrs) {
   let application;
 
   // use defaults, but you can override
-  let attributes = Ember.assign({}, config.APP, attrs);
+  // Ember.assign is not supported on Ember < 2.5
+  let attributes = merge({}, config.APP);
+  merge(attributes, attrs);
 
-  Ember.run(() => {
+  run(() => {
     application = Application.create(attributes);
     application.setupForTesting();
     application.injectTestHelpers();
