63e3bf98093922f2a590c37743901a20d3671ebf
Steve Calvert <scalvert@linkedin.com>
2017-07-30 13:29:53 -0700

    Upgrading addon using ember init

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,9 +1,7 @@
 import Ember from 'ember';
 import config from './config/environment';
 
-const { Router: EmberRouter } = Ember;
-
-const Router = EmberRouter.extend({
+const Router = Ember.Router.extend({
   location: config.locationType,
   rootURL: config.rootURL
 });
--- tests/helpers/start-app.js
+++ tests/helpers/start-app.js
@@ -2,21 +2,14 @@ import Ember from 'ember';
 import Application from '../../app';
 import config from '../../config/environment';
 
-const { merge, run } = Ember;
-
 export default function startApp(attrs) {
-  let application;
+  let attributes = Ember.merge({}, config.APP);
+  attributes = Ember.merge(attributes, attrs); // use defaults, but you can override;
 
-  // use defaults, but you can override
-  // Ember.assign is not supported on Ember < 2.5
-  let attributes = merge({}, config.APP);
-  merge(attributes, attrs);
-
-  run(() => {
-    application = Application.create(attributes);
+  return Ember.run(() => {
+    let application = Application.create(attributes);
     application.setupForTesting();
     application.injectTestHelpers();
+    return application;
   });
-
-  return application;
 }
