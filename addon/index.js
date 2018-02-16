0809fee830c70339f72ec61fc4bd41d3958aa393
Steve Calvert <scalvert@linkedin.com>
2018-02-16 10:42:52 -0800

    Adds exports

--- addon/index.js
+++ addon/index.js
@@ -1,7 +1,9 @@
 export { runTask, scheduleTask, throttleTask, cancelTask } from './run-task';
 export { pollTask, pollTaskFor, setShouldPoll, cancelPoll } from './poll-task';
 export { debounceTask, cancelDebounce } from './debounce-task';
+export { addEventListener, removeEventListener } from './dom-event-listeners';
 export { runDisposables } from './utils/disposable';
+
 export { ContextBoundTasksMixin } from './mixins/run';
 export { ContextBoundEventListenersMixin } from './mixins/dom';
 export { DisposableMixin } from './mixins/disposable';
