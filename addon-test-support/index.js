810200cbce354f99c0c9e71a77782507f5c1e972
Steve Calvert <scalvert@linkedin.com>
2018-03-06 08:28:56 -0800

    Fixing a few things based on feedback

--- addon-test-support/index.js
+++ addon-test-support/index.js
@@ -1,11 +1,13 @@
-import QUnit from 'qunit';
+import require from 'require';
 import { hasRunAllDisposables } from 'ember-lifeline';
+
+let QUnit = require('qunit').default;
 
 const FAILED_ASSERTION_MESSAGE =
   'One or more objects registered disposables that were not correctly disposed of. Please ensure that objects correctly run their registered disposables by calling `runDisposables` in the `destroy` method of the object.';
 let setupTestDone = false;
 
-export default function setupEnsureLifelineDisposablesRun(hooks) {
+export default function setupLifelineValidation(hooks) {
   if (!setupTestDone) {
     QUnit.testDone(ensureDisposablesRunIfNoFailures);
 
@@ -16,8 +18,8 @@ export default function setupEnsureLifelineDisposablesRun(hooks) {
 }
 
 export function ensureDisposablesRunIfNoFailures(details) {
-  if (details.failed === 0) {
-    ensureDisposablesRun();
+  if (details.failed === 0 && !hasRunAllDisposables()) {
+    throw new Error(FAILED_ASSERTION_MESSAGE);
   }
 }
 
