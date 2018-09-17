9d987fdb4b5187d6242cec6a201437c2880dc7f6
Mehul Kar <mehul.kar@gmail.com>
2018-09-17 13:43:49 -0700

    Assign registeredDisposables to Map beforeEach test

---
+++ tests/acceptance/cleanup-test.js
@@ -0,0 +1,19 @@
+import { module, test } from 'qunit';
+import { setupApplicationTest } from 'ember-qunit';
+import { visit, currentURL } from '@ember/test-helpers';
+import setupLifelineValidation from 'ember-lifeline/test-support';
+
+module('Acceptance | cleaning up disposables', function(hooks) {
+  setupLifelineValidation(hooks);
+  setupApplicationTest(hooks);
+
+  test('visiting /', async function(assert) {
+    await visit('/');
+    assert.equal(currentURL(), '/');
+  });
+
+  test('visiting /', async function(assert) {
+    await visit('/');
+    assert.equal(currentURL(), '/');
+  });
+});
