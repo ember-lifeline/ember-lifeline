7017971826504a49daffe075d866d2aacedc7ece
Jan Buschtöns <jan.buschtoens@clark.de>
2018-11-06 21:38:38 +0100

    chore(build): use Promise fix instead of polyfill

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,11 +4,7 @@
 const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
-  let options = {
-    'ember-cli-babel': {
-      includePolyfill: true
-    }
-  };
+  let options = {};
 
   if (defaults.project.findAddonByName('ember-native-dom-event-dispatcher')) {
     options.vendorFiles = { 'jquery.js': null };
@@ -22,6 +18,8 @@ module.exports = function(defaults) {
     This build file does *not* influence how the addon or the app using it
     behave. You most likely want to be modifying `./index.js` or app's build file
   */
+
+  app.import({ test: 'vendor/fix-promise.js' });
 
   return app.toTree();
 };
---
+++ vendor/fix-promise.js
@@ -0,0 +1,2 @@
+// https://github.com/babel/ember-cli-babel/issues/250
+self.Promise = self.Promise || Ember.RSVP.Promise;
