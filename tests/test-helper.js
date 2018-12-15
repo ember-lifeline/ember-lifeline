935b0f20febc95388693e2e70ebdf76417aee55d
Steve Calvert <scalvert@linkedin.com>
2018-12-15 12:18:01 -0800

    Converting from ember-cli-qunit to ember-qunit

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -3,7 +3,7 @@ import QUnit from 'qunit';
 import Application from '../app';
 import config from '../config/environment';
 import { setApplication } from '@ember/test-helpers';
-import { start } from 'ember-cli-qunit';
+import { start } from 'ember-qunit';
 
 setApplication(Application.create(config.APP));
 start();
