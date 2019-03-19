0a448f9257bdf20e3c93c4a50b4defce9f9ca241
Steve Calvert <scalvert@linkedin.com>
2019-03-19 09:47:51 -0700

    Adding ember-cli-addon-docs

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,11 +1,17 @@
-import EmberRouter from '@ember/routing/router';
+import AddonDocsRouter, { docsRoute } from 'ember-cli-addon-docs/router';
+import RouterScroll from 'ember-router-scroll';
 import config from './config/environment';
 
-const Router = EmberRouter.extend({
+const Router = AddonDocsRouter.extend(RouterScroll, {
   location: config.locationType,
   rootURL: config.rootURL,
 });
 
-Router.map(function() {});
+Router.map(function() {
+  docsRoute(this, function() {
+    /* Your docs routes go here */
+  });
+  this.route('not-found', { path: '/*path' });
+});
 
 export default Router;
